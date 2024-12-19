cab9a7cea54595baf112d99de5da967ced36e6cf
Strong649 <st20121704@outlook.cardiffmet.ac.uk>
2024-12-19 13:51:43 +0000

    Update Beijing_app.py

--- Beijing_app.py
+++ Beijing_app.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+import plotly.graph_objects as go
 from sklearn.model_selection import train_test_split
 import folium
 from streamlit_folium import st_folium
